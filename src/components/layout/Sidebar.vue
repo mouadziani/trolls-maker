a36fa6d26d7e3d01b1fb66fa174ad36fdc2f1aa6
MouadZIANI <mouad.ziani1997@gmail.com>
2021-03-19 02:38:22 +0100

    feat(ui): added basic layout design

---
+++ src/components/layout/Sidebar.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="flex bg-gray-800 text-gray-200">
+    <div class="flex flex-col h-full w-24 items-center overflow-y-scroll">
+      <a
+        class="border-b border-gray-700 w-full flex flex-col items-center py-4 text-xs hover:bg-gray-600"
+        href="#"
+      >
+        <svg
+          class="w-6 h-6 mb-1"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+        <span>Layout</span>
+      </a>
+      <a
+        class="border-b border-gray-600 w-full flex flex-col items-center py-4 text-xs hover:bg-gray-600"
+        href="#"
+      >
+        <svg
+          class="w-6 h-6 mb-1"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+        <span>Elements</span>
+      </a>
+      <a
+        class="border-b border-gray-600 w-full flex flex-col items-center py-4 text-xs hover:bg-gray-600"
+        href="#"
+      >
+        <svg
+          class="w-6 h-6 mb-1"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+        <span>Text</span>
+      </a>
+      <a
+        class="w-full flex flex-col items-center py-4 text-xs hover:bg-gray-600"
+        href="#"
+      >
+        <svg
+          class="w-6 h-6 mb-1"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+          />
+        </svg>
+        <span>Images</span>
+      </a>
+    </div>
+    <div class="w-96 h-full px-4 py-6 overflow-y-scroll">
+      <div class="relative mb-6">
+        <svg
+          class="absolute top-3 left-3 text-gray-400 block h-5 w-5"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
+        <input
+          type="text"
+          placeholder="Search template"
+          class="text-sm w-full px-5 py-3 pl-10 rounded-full text-black font-medium bg-white placeholder-gray-900"
+        >
+      </div>
+      <div class="grid grid-cols-2 gap-4">
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+        <img
+          class="w-full h-56"
+          src="https://via.placeholder.com/150"
+          alt="template"
+        >
+      </div>
+    </div>
+  </div>
+</template>
